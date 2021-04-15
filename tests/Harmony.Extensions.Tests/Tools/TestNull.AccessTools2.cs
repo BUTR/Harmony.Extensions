b4ad8618e1cf919f90e0bd806d3cfc50c5c453bf
Vitaly Mikhailov <personal@aragas.org>
2021-04-15 17:04:28 +0300

    More nulls

--- tests/Harmony.Extensions.Tests/Tools/TestNull.AccessTools2.cs
+++ tests/Harmony.Extensions.Tests/Tools/TestNull.AccessTools2.cs
@@ -33,6 +33,7 @@ namespace HarmonyLibTests.Tools
         [Test]
         public void Test_AccessTools2_Fields()
         {
+            Assert.IsNull(AccessTools2.DeclaredField(typeof(TestClass), null!));
             Assert.IsNull(AccessTools2.DeclaredField(null!, null!));
             Assert.IsNull(AccessTools2.Field(null!, null!));
             Assert.IsNull(AccessTools2.Field(null!));
@@ -41,16 +42,22 @@ namespace HarmonyLibTests.Tools
         [Test]
         public void Test_AccessTools2_Properties()
         {
+            Assert.IsNull(AccessTools2.DeclaredProperty(typeof(TestClass), null!));
             Assert.IsNull(AccessTools2.DeclaredProperty(null!, null!));
+            Assert.IsNull(AccessTools2.Property(typeof(TestClass), null!));
             Assert.IsNull(AccessTools2.Property(null!, null!));
             Assert.IsNull(AccessTools2.Property(null!));
 
+            Assert.IsNull(AccessTools2.DeclaredPropertyGetter(typeof(TestClass), null!));
             Assert.IsNull(AccessTools2.DeclaredPropertyGetter(null!, null!));
+            Assert.IsNull(AccessTools2.DeclaredPropertySetter(typeof(TestClass), null!));
             Assert.IsNull(AccessTools2.DeclaredPropertySetter(null!, null!));
 
+            Assert.IsNull(AccessTools2.PropertyGetter(typeof(TestClass), null!));
             Assert.IsNull(AccessTools2.PropertyGetter(null!, null!));
             Assert.IsNull(AccessTools2.PropertyGetter(null!));
 
+            Assert.IsNull(AccessTools2.PropertySetter(typeof(TestClass), null!));
             Assert.IsNull(AccessTools2.PropertySetter(null!, null!));
             Assert.IsNull(AccessTools2.PropertySetter(null!));
         }
@@ -58,7 +65,9 @@ namespace HarmonyLibTests.Tools
         [Test]
         public void Test_AccessTools2_Methods()
         {
+            Assert.IsNull(AccessTools2.DeclaredMethod(typeof(TestClass), null!));
             Assert.IsNull(AccessTools2.DeclaredMethod(null!, null!));
+            Assert.IsNull(AccessTools2.Method(typeof(TestClass), null!));
             Assert.IsNull(AccessTools2.Method(((Type) null)!, null!));
             Assert.IsNull(AccessTools2.Method(null!));
         }
@@ -67,15 +76,22 @@ namespace HarmonyLibTests.Tools
         public void Test_AccessTools2_Delegates()
         {
             Assert.IsNull(AccessTools2.GetDeclaredConstructorDelegate<TestDelegate>(null!));
+            Assert.IsNull(AccessTools2.GetDeclaredDelegate<TestDelegate>(typeof(TestClass), null!));
             Assert.IsNull(AccessTools2.GetDeclaredDelegate<TestDelegate>(null!, null!));
+            Assert.IsNull(AccessTools2.GetDeclaredDelegate<TestDelegate>(typeof(TestClass), null!, null, null));
             Assert.IsNull(AccessTools2.GetDeclaredDelegate<TestDelegate>(null!, null!, null, null));
+            Assert.IsNull(AccessTools2.GetDeclaredDelegateObjectInstance<TestDelegate>(typeof(TestClass), null!));
             Assert.IsNull(AccessTools2.GetDeclaredDelegateObjectInstance<TestDelegate>(null!, null!));
 
             Assert.IsNull(AccessTools2.GetDelegate<TestDelegate>(((ConstructorInfo) null)!));
             Assert.IsNull(AccessTools2.GetDelegate<TestDelegate>(((MethodInfo) null)!));
+            Assert.IsNull(AccessTools2.GetDelegate<TestDelegate>(typeof(TestClass), null!));
             Assert.IsNull(AccessTools2.GetDelegate<TestDelegate>(((Type) null)!, null!));
+            Assert.IsNull(AccessTools2.GetDelegate<TestDelegate>(typeof(TestClass), null!, null!, null!));
             Assert.IsNull(AccessTools2.GetDelegate<TestDelegate>(null!, null!, null!, null!));
+            Assert.IsNull(AccessTools2.GetDelegate<TestDelegate>(new object(), null!));
             Assert.IsNull(AccessTools2.GetDelegate<TestDelegate>((object) null, null!));
+            Assert.IsNull(AccessTools2.GetDelegate<TestDelegate>(new object(), null!, null!, null!));
             Assert.IsNull(AccessTools2.GetDelegate<TestDelegate>((object) null, null!, null!, null!));
             Assert.IsNull(AccessTools2.GetDelegate<TestDelegate>(((string) null)!));
         }
@@ -86,6 +102,7 @@ namespace HarmonyLibTests.Tools
             Assert.IsNull(AccessTools2.FieldRefAccess<TestClass, TestDelegate>(((FieldInfo) null)!));
             Assert.IsNull(AccessTools2.FieldRefAccess<TestClass, TestDelegate>(((string) null)!));
             Assert.IsNull(AccessTools2.FieldRefAccess<TestDelegate>(((FieldInfo) null)!));
+            Assert.IsNull(AccessTools2.FieldRefAccess<TestDelegate>(typeof(TestClass), null!));
             Assert.IsNull(AccessTools2.FieldRefAccess<TestDelegate>(null!, null!));
         }
 
@@ -93,6 +110,7 @@ namespace HarmonyLibTests.Tools
         public void Test_AccessTools2_StaticFieldRef()
         {
             Assert.IsNull(AccessTools2.StaticFieldRefAccess<TestDelegate>(((FieldInfo) null)!));
+            Assert.IsNull(AccessTools2.StaticFieldRefAccess<TestDelegate>(typeof(TestClass), null!));
             Assert.IsNull(AccessTools2.StaticFieldRefAccess<TestDelegate>(null!, null!));
         }
 
